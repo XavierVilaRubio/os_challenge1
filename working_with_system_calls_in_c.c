83d7714e8cfbc968495696c3f41ecca50600498c
Xavier Vila Rubio <xvrvilarubio2001gmail.com>
2020-09-29 11:10:43 +0200

    Enchanced working_with_system_calls_in_c.c

--- working_with_system_calls_in_c.c
+++ working_with_system_calls_in_c.c
@@ -1,29 +1,50 @@
 #include<stdio.h>
 #include<sys/time.h>
 #include<sys/types.h>
+#include<string.h>
 
-int main(){
+int main(int argc, char *argv[]){
+
+    struct timeval start, end;
+
     int dummy_function(){
 	return 3;
     }
 
-    struct timeval start, end;
+    void system_call(){
+	printf("SYSTEM CALL\n");
 
-    gettimeofday (&start, NULL);
-    for(int i=0; i<1000000; i++){
-	pid_t getpid(void);
+	gettimeofday (&start, NULL);
+	for(int i=0; i<1000000; i++){
+	    pid_t getpid(void);
+	}
+	gettimeofday (&end, NULL);
+
+	printf("Time in microseconds: %d microseconds\n", end.tv_usec - start.tv_usec);
     }
-    gettimeofday (&end, NULL);
 
-    printf("Time in microseconds: %d microseconds\n", end.tv_usec - start.tv_usec);
+    void procedure_call(){
+	printf("PROCEDURE CALL\n");
 
-    gettimeofday (&start, NULL);
-    for(int i=0; i<1000000; i++){
-	dummy_function();
+	gettimeofday (&start, NULL);
+	for(int i=0; i<1000000; i++){
+	    dummy_function();
+	}
+	gettimeofday (&end, NULL);
+
+	printf("Time in microseconds: %d microseconds\n", end.tv_usec - start.tv_usec);
     }
-    gettimeofday (&end, NULL);
 
-    printf("Time in microseconds: %d microseconds\n", end.tv_usec - start.tv_usec);
+    if(argc == 2){
+	if(strcmp(argv[1], "sys") == 0){
+	    system_call();
+	}else if(strcmp(argv[1], "proc") == 0){
+	    procedure_call();
+	}
+    }else{
+	system_call();
+	procedure_call();
+    }
 
     return 0;
 }
